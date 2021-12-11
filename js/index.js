d515dde4bc25a81b0c0e07a7a98905ae6cdeb606
Parth Dahiya <parth.dahiya01@gmail.com>
2021-12-11 14:10:08 +0530

    upto lightbox

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+ $('.btn-minuse').on('click', function(){
+    $(this).parent().siblings('input').val(parseInt($(this).parent().siblings('input').val()) - 1)
+});
+
+$('.btn-pluss').on('click', function(){
+    $(this).parent().siblings('input').val(parseInt($(this).parent().siblings('input').val()) + 1)
+});
+
+$(function () {
+    $('[data-toggle="popover"]').popover({
+        placement : 'top',
+		trigger : 'click',
+        container: 'body',
+        html : true,
+        content : '<div class="media"><img src="../images/image-product-1-thumbnail.jpg" class="mr-3" alt="Sample Image"><div class="media-body"><p>Fall Limited Edition Sneakers <br> $125*3 <strong>$375</strong></strong></p><br></div></div><div><a href="#" class="btn checkout">Checkout</a></div>'
+    });
+  });
+$('.popover-dismiss').popover({
+    trigger: 'focus'
+  });
